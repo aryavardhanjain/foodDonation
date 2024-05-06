9494120edcbdd311b4b4e3e914122e8ab0822233
aryavardhanjain <jainaryan418@gmail.com>
2024-05-07 00:53:10 +0530

    Nearing Finalization

--- organization/serializers.py
+++ organization/serializers.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
-    date = serializers.DateTimeField(format='%d-%m-%Y', input_formats=['%d-%m-%Y', 'iso-8601'])
-    time = serializers.DateTimeField(format='%H:%M:%S', input_formats=['%H:%M:%S', 'iso-8601'])
     
     class Meta:
         model = Event
