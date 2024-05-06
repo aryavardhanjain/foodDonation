f298e270fe4287a09988b5c0a62abcc812258ea6
aryavardhanjain <jainaryan418@gmail.com>
2024-05-07 00:54:36 +0530

    Nearing Finalization

--- organization/serializers.py
+++ organization/serializers.py
@@ -5,5 +5,5 @@ class EventSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Event
-        fields = ['id', 'organization', 'title', 'description', 'date', 'time', 'location', 'latitude', 'longitude', 'created_at', 'updated_at']
+        fields = ['id', 'organization', 'title', 'description', 'date', 'time', 'location', 'latitude', 'longitude', 'volunteer_count', 'created_at', 'updated_at']
         read_only_fields = ['organization', 'created_at', 'updated_at']
